d08b545758d11b9ed5aa991ea25671f8604aaedf
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-12-16 13:36:52 -0500

    Add methods un the users, v1, api

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -32,3 +32,52 @@ class UserList(Resource):
             'last_name': new_user.last_name,
             'email': new_user.email
         }, 201
+
+    @api.response(200, 'Users retrieved successfully')
+    def get(self):
+        """Get list of users"""
+        users = facade.get_all_users()
+
+        return [
+            {
+                'id': user.id,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'email': user.email
+            }
+            for user in users
+        ], 200
+
+@api.route('/<user_id>')
+class UserResource(Resource):
+    @api.response(200, 'User details retrieved successfully')
+    @api.response(404, 'User not found')
+    def get(self, user_id):
+        """Get user details by ID"""
+        user = facade.get_user(user_id)
+        if not user:
+            return {'error': 'User not found'}, 404
+        return {
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email
+            }, 200
+
+    @api.expect(user_model, validate=True)
+    @api.response(200, 'User updated successfully')
+    @api.response(404, 'User not found')
+    def put(self, user_id):
+        """Update user"""
+        user_data = api.payload
+
+        updated_user = facade.update_user(user_id, user_data)
+        if not updated_user:
+            return {'error': 'User not found'}, 404
+
+        return {
+            'id': updated_user.id,
+            'first_name': updated_user.first_name,
+            'last_name': updated_user.last_name,
+            'email': updated_user.email
+        }, 200
