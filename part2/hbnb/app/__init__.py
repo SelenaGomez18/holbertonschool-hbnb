31ee56bac0e0cf11c15603f479f91f54514c9475
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-12-18 19:22:03 -0500

    Update facade

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -21,8 +21,8 @@ def create_app():
         prefix='/api/v1'
         )
 
-    api.add_namespace(users_ns, path="/api/v1/users")
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    api.add_namespace(places_ns, path='/api/v1/places')
-    
+    api.add_namespace(users_ns, path="/users")
+    api.add_namespace(amenities_ns, path="/amenities")
+    api.add_namespace(places_ns, path="/places")
+
     return app
