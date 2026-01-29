40274826a807e7edf55c244cff5b0fe411f47ab6
macxiliam <alexanderbiman@gmail.com>
2026-01-29 18:02:16 -0500

    Update the User model

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
-
+from flask_restx import Api
+from hbnb.app.extensions import bcrypt
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    bcrypt.init_app(app)
     return app
